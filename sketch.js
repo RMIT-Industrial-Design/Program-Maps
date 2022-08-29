d8be8e1fa0231ddf9a298452367645c28a302c9f
Scott Mitchell <scott@openobject.org>
2022-08-30 09:47:06 +1000

    files from p5js web editor

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let courses = [];
+let tableOffset = 30;
+let rowHeight = 130;
+let rowPadding = 4;
+let cellPadding = 4;
+let numYears;
+
+function preload() {
+  courseData = loadTable("courseData.csv", "csv", "header");
+}
+
+function setup() {
+  createCanvas(1000, 800);
+  
+  loadCourseDetails();
+  
+  updateAvailableCourses();
+}
+
+function draw() {
+  background(255);
+  // draw year labels
+  drawYearLabels(numYears, rowHeight, tableOffset, rowPadding, cellPadding);
+  // draw courses
+  for (let i = 0; i < courses.length; i++) {
+    courses[i].show();
+  }
+}
+
+function mousePressed() {
+  let courseChange = false;
+  for (let i = 0; i < courses.length; i++) {
